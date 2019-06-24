c91ab3dfcc610deebd4bb0faded65ffa8870c494
谷慧 <879910950@qq.com>
2019-06-24 18:20:53 +0800

    修改

--- src/chromeDevTools/test1.js
+++ src/chromeDevTools/test1.js
@@ -1,32 +1,42 @@
-
+// 点击按钮
 function onClick() {
-    if (inputsAreEmpty()) {
-      label.textContent = 'Error: one or both inputs are empty.';
-      return;
-    }
-    updateLabel();
+  // 检查输入框中的值是否为空
+  if (inputsAreEmpty()) {
+    label.textContent = "错误: 输入框中的值为空.";
+    return;
   }
-  function inputsAreEmpty() {
-    if (getNumber1() === '' || getNumber2() === '') {
-      return true;
-    } else {
-      return false;
-    }
+  // throw "error";
+  // 计算相加的结果，并更新DOM
+  updateLabel();
+}
+
+// 检查输入框中的值是否为空
+function inputsAreEmpty() {
+  if (getNumber1() === "" || getNumber2() === "") {
+    return true;
+  } else {
+    return false;
   }
-  function updateLabel() {
-    var addend1 = getNumber1();
-    var addend2 = getNumber2();
-    var sum = addend1 + addend2;
-    label.textContent = addend1 + ' + ' + addend2 + ' = ' + sum;
-  }
-  function getNumber1() {
-    return inputs[0].value;
-  }
-  function getNumber2() {
-    return inputs[1].value;
-  }
-  var inputs = document.querySelectorAll('input');
-  var label = document.querySelector('p');
-  var button = document.querySelector('button');
-  button.addEventListener('click', onClick);
-  
+}
+
+// 计算相加的结果，并更新DOM
+function updateLabel() {
+  var addend1 = getNumber1();
+  var addend2 = getNumber2();
+  var sum = addend1 + addend2;
+  label.textContent = addend1 + " + " + addend2 + " = " + sum;
+}
+
+// 获取第一个输入框中的值
+function getNumber1() {
+  return inputs[0].value;
+}
+// 获取第二个输入框中的值
+function getNumber2() {
+  return inputs[1].value;
+}
+
+var inputs = document.querySelectorAll("input");
+var label = document.querySelector("p");
+var button = document.querySelector("button.add");
+button.addEventListener("click", onClick);
