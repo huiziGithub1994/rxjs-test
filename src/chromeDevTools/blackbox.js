3a8af3209fade7999a42afc29bfe40150214d0e8
谷慧 <879910950@qq.com>
2019-06-25 10:56:22 +0800

    新增snippets

--- src/chromeDevTools/blackbox.js
+++ src/chromeDevTools/blackbox.js
@@ -1,14 +1,12 @@
-import moment from 'moment'
 /**
  * 如果要在调试时忽略该脚本，请使用Blackbox脚本。黑盒子时，“调用堆栈”窗格中的脚本会被遮挡，当您单步执行代码时，您永远不会进入脚本的功能。
  */
-import {imgCompress} from '../utils/helper'
+import { imgCompress } from "../utils/helper";
 
-
-function onClick(){
-    const cover = 'file/201905/11/20190511154219099126464.png';
-    const path = imgCompress(cover)
-    document.querySelector('img.cover').setAttribute('src',path)
+function onClick() {
+  const cover = "file/201905/11/20190511154219099126464.png";
+  const path = imgCompress(cover);
+  document.querySelector("img.cover").setAttribute("src", path);
 }
 const button = document.querySelector("button.showImg");
 button.addEventListener("click", onClick);
--- src/chromeDevTools/xhr.js
+++ src/chromeDevTools/xhr.js
@@ -1,8 +1,8 @@
-  //   
-  fetch('http://47.107.255.128:8089/zxx/visitor/picture/createValidateCode')
+//
+fetch("http://47.107.255.128:8089/zxx/visitor/picture/createValidateCode")
   .then(function(response) {
-      return response.json(); // parses response to JSON
-    })
-    .then(function(myJson) {
-      document.querySelector('img.code').setAttribute('src',myJson.url)
-    });
+    return response.json(); // parses response to JSON
+  })
+  .then(function(myJson) {
+    document.querySelector("img.code").setAttribute("src", myJson.DATA);
+  });
