205d71f75384c08f252146fa9f7794fd19b379f1
guhui <879910950@qq.com>
2019-06-25 23:03:08 +0800

    增加注释

--- src/chromeDevTools/blackbox.js
+++ src/chromeDevTools/blackbox.js
@@ -1,12 +1,25 @@
 /**
- * 如果要在调试时忽略该脚本，请使用Blackbox脚本。黑盒子时，“调用堆栈”窗格中的脚本会被遮挡，当您单步执行代码时，您永远不会进入脚本的功能。
+ * 1、如果要在调试时忽略该脚本，请使用Blackbox脚本。黑盒子时，“调用堆栈”窗格中的脚本会被遮挡，
+ *    当您单步执行代码时，您永远不会进入脚本的功能。
+ * 
+ * 2、我们还可以给 DOM 元素设置断点。因为我们有时候需要监听和查看某个元素的变化、赋值情况，
+ *    但是并是不太关心哪一段代码对它做的修改，只想看看它的变化情况，那么可以给它来个监听事件
+ *    DOM Breakpoints的类型：
+ *      a、subtree modifications:子节点属性修改
+ *      b、attribute modifications:自身属性修改
+ *      c、node removal: 自身节点被删除
  */
-import { imgCompress } from "../utils/helper";
+import { imgCompress ,isOpenInPc} from "../utils/helper";
 
 function onClick() {
+  if(isOpenInPc()){
+    document.querySelector('p.coverTip').textContent = "提示：请在移动端打开图片"
+  }
   const cover = "file/201905/11/20190511154219099126464.png";
   const path = imgCompress(cover);
-  document.querySelector("img.cover").setAttribute("src", path);
+  const img = document.querySelector("img.cover");
+  img.setAttribute("srs", path);
 }
+
 const button = document.querySelector("button.showImg");
 button.addEventListener("click", onClick);
--- src/chromeDevTools/xhr.js
+++ src/chromeDevTools/xhr.js
@@ -1,8 +1,9 @@
-//
+// 获取验证码
 fetch("http://47.107.255.128:8089/zxx/visitor/picture/createValidateCode")
   .then(function(response) {
     return response.json(); // parses response to JSON
   })
   .then(function(myJson) {
-    document.querySelector("img.code").setAttribute("src", myJson.DATA);
+    const {url} = myJson
+    document.querySelector("img.code").setAttribute("src",url);
   });
